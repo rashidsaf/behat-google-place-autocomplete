3a6f2113303b28adb02c9a49a01032eadfecb6ad
Donald Tyler <domhnall.tilliere@gmail.com>
2017-11-06 09:00:03 -0600

    Update PHP CLI to Chekote 7.1.10

--- docker/lib/images.sh
+++ docker/lib/images.sh
@@ -5,3 +5,9 @@ export NODE_ACCOUNT=chekote
 export NODE_REPO=node
 export NODE_TAG=9.0.0
 export NODE_IMAGE=${NODE_HOST}/${NODE_ACCOUNT}/${NODE_REPO}:${NODE_TAG}
+
+export PHP_HOST=registry.hub.docker.com
+export PHP_ACCOUNT=chekote
+export PHP_REPO=php
+export PHP_TAG=7.1.10-behat
+export PHP_IMAGE=${PHP_HOST}/${PHP_ACCOUNT}/${PHP_REPO}:${PHP_TAG}
