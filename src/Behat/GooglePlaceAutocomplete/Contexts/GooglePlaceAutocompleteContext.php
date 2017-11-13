c014b87199521234c07da52c8fd2809bae511c0c
Donald Tyler <domhnall.tilliere@gmail.com>
2017-11-12 18:31:11 -0600

    Updated src to new Behat standards
    I thought my last PR had already done this, but apparently I had only updated the bootstrap Context >.<
    
    I converted the GooglePlaceAutocompleteContext from a trait to a class, and updated the related config/classes accordingly.

--- src/Behat/GooglePlaceAutocomplete/Contexts/GooglePlaceAutocompleteContext.php
+++ src/Behat/GooglePlaceAutocomplete/Contexts/GooglePlaceAutocompleteContext.php
@@ -1,28 +1,38 @@
 <?php namespace Medology\Behat\GooglePlaceAutocomplete\Contexts;
 
-use Behat\FlexibleMink\PseudoInterface\SpinnerContextInterface;
+use Behat\Behat\Context\Context;
 use Behat\Mink\Exception\ExpectationException;
-use Medology\Behat\GooglePlaceAutocomplete\PseudoInterfaces\MinkContextInterface;
+use Medology\Behat\Mink\UsesFlexibleContext;
+use Medology\Spinner;
 
 /**
- * {@inheritdoc}
+ * A context for handling tests for the Gooogle Place Autocomplete Javascript script.
  */
-trait GooglePlaceAutocompleteContext
+class GooglePlaceAutocompleteContext implements Context
 {
-    use MinkContextInterface;
-    use SpinnerContextInterface;
+    use UsesFlexibleContext;
 
     /**
-     * {@inheritdoc}
-     * @When /^(?:I |)choose the first place autocomplete option$/i
+     * Chooses the first address in the autocomplete popup.
+     *
+     * @When   /^(?:I |)choose the first place autocomplete option$/i
+     * @throws ExpectationException If the first address is not found
      */
     public function chooseFirstOption()
     {
-        $pac_first_item = $this->waitFor(function () {
-            $pac_first_item = $this->getSession()->getPage()->find('css', '.pac-container>.pac-item:first-child');
+        $pac_first_item = Spinner::waitFor(function () {
+            $pac_first_item = $this->flexibleContext->getSession()->getPage()->find(
+                'css',
+                '.pac-container>.pac-item:first-child'
+            );
+
             if (!$pac_first_item) {
-                throw new ExpectationException('Could not find the first place autocomplete item', $this->getSession());
+                throw new ExpectationException(
+                    'Could not find the first place autocomplete item',
+                    $this->flexibleContext->getSession()
+                );
             }
+
             return $pac_first_item;
         });
 
