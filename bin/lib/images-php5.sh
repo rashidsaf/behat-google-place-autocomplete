6e256519f539af2d509205bfbd70048e6df0ea29
Timothy Bordelon <red@lyrawearspants.com>
2019-03-15 16:29:05 -0500

    Set up CI to run against PHP 5.6 and 7.2

---
+++ bin/lib/images-php5.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export PHP_ACCOUNT=chekote
+export PHP_REPO=php
+export PHP_TAG=5.6.40-behat3.4
---
+++ bin/lib/images.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export PHP_ACCOUNT=chekote
+export PHP_REPO=php
+export PHP_TAG=7.2.14-behat3.4
