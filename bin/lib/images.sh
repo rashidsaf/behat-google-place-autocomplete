0f2d748de41a43503864c731f031440d9dfd041a
Timothy Bordelon <red@lyrawearspants.com>
2019-03-19 18:32:17 -0500

    Code review changes

--- bin/lib/images.sh
+++ bin/lib/images.sh
@@ -2,4 +2,11 @@
 
 export PHP_ACCOUNT=chekote
 export PHP_REPO=php
-export PHP_TAG=7.2.14-behat3.4
+if [ -z "${USE_PHP5}" ]; then
+    export PHP_TAG=7.2.14-behat3.4
+else
+    export PHP_TAG=5.6.40-behat3.4
+fi
+
+export SED_REPO=alpine
+export SED_TAG=3.8
