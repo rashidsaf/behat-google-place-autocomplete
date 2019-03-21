4947162016b3a59732190a7171b03b7a34410ef3
Timothy Bordelon <red@lyrawearspants.com>
2019-03-21 14:50:40 -0500

    Update image tags

--- bin/lib/images.sh
+++ bin/lib/images.sh
@@ -3,9 +3,9 @@
 export PHP_ACCOUNT=chekote
 export PHP_REPO=php
 if [ -z "${USE_PHP5}" ]; then
-    export PHP_TAG=7.2.14-behat3.4
+    export PHP_TAG=7.2.16-behat3.4
 else
-    export PHP_TAG=5.6.40-behat3.4
+    export PHP_TAG=5.6.40.a-behat3.4
 fi
 
 export SED_REPO=alpine
