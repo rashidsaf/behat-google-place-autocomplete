b3d1da892b487d2d523c543c20baae8e58a11e6e
rashid <rashid@healthlabs.com>
2019-11-01 14:55:45 -0500

    Fix for Selenium causing intermittenly failing test

--- src/Behat/GooglePlaceAutocomplete/Contexts/GooglePlaceAutocompleteContext.php
+++ src/Behat/GooglePlaceAutocomplete/Contexts/GooglePlaceAutocompleteContext.php
@@ -2,6 +2,7 @@
 
 use Behat\FlexibleMink\PseudoInterface\SpinnerContextInterface;
 use Behat\Mink\Exception\ExpectationException;
+use Exception;
 use Medology\Behat\GooglePlaceAutocomplete\PseudoInterfaces\MinkContextInterface;
 
 /**
@@ -18,14 +19,16 @@ trait GooglePlaceAutocompleteContext
      */
     public function chooseFirstOption()
     {
-        $pac_first_item = $this->waitFor(function () {
-            $pac_first_item = $this->getSession()->getPage()->find('css', '.pac-container>.pac-item:first-child');
-            if (!$pac_first_item) {
-                throw new ExpectationException('Could not find the first place autocomplete item', $this->getSession());
+        $this->waitFor(function () {
+            try {
+                $pac_first_item = $this->getSession()->getPage()->find('css', '.pac-container>.pac-item:first-child');
+                if (!$pac_first_item) {
+                    throw new ExpectationException('Could not find the first place autocomplete item', $this->getSession());
+                }
+                $pac_first_item->click();
+            } catch (Exception $e) {
+                throw new ExpectationException($e->getMessage(), $this->getSession());
             }
-            return $pac_first_item;
         });
-
-        $pac_first_item->click();
     }
 }
